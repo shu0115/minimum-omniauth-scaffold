a795076949a0914eb6b35ea969f84c38dc106688
shu0115 <s.matsumoto0115@gmail.com>
2013-03-27 11:56:19 +0900

    First commit

---
+++ lib/minimum/omniauth/scaffold.rb
@@ -0,0 +1,9 @@
+require "minimum/omniauth/scaffold/version"
+
+module Minimum
+  module Omniauth
+    module Scaffold
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/minimum/omniauth/scaffold/version.rb
@@ -0,0 +1,7 @@
+module Minimum
+  module Omniauth
+    module Scaffold
+      VERSION = "0.0.1"
+    end
+  end
+end
